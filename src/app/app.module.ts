e476c984c409cad45e20bebcfc4d5f17dc777d53
Steve Witman <stevewitman@gmail.com>
2017-04-17 19:59:01 -0600

    Generate empty service, components, and Item class

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ItemsListComponent } from './items/items-list/items-list.component';
+import { ItemDetailComponent } from './items/item-detail/item-detail.component';
+import { ItemFormComponent } from './items/item-form/item-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemsListComponent,
+    ItemDetailComponent,
+    ItemFormComponent
   ],
   imports: [
     BrowserModule,
